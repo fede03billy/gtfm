1f145196794f52c347734a4fa9aad0c9217e956d
Federico C <fede03billy@gmail.com>
2023-03-17 15:41:15 +0100

    🪲 Build fix

--- pages/api/restaurant/info/[restaurant_id].js
+++ pages/api/restaurant/info/[restaurant_id].js
@@ -3,7 +3,7 @@ import databaseConnection from '../../../../util/databaseConnection.js';
 
 export default async function handler(req, res) {
   // Get the restaurant_id from the query parameter
-  const restaurant_id = req.query?.restaurant_id;
+  let restaurant_id = req.query?.restaurant_id;
   // clean restaurant_id of the quotes and spaces
   restaurant_id = restaurant_id.replace(/['"]+/g, '').trim();
   try {
