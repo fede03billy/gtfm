28e91068c896531d787fc4db0b929fcefa60a168
Federico C <fede03billy@gmail.com>
2023-03-17 15:37:36 +0100

    🪲 Api route for restaurant info fixed

--- pages/api/restaurant/info/[restaurant_id].js
+++ pages/api/restaurant/info/[restaurant_id].js
@@ -1,14 +1,13 @@
 import Restaurant from '../../../../models/restaurant.js';
 import databaseConnection from '../../../../util/databaseConnection.js';
 
-databaseConnection();
-
 export default async function handler(req, res) {
   // Get the restaurant_id from the query parameter
   const restaurant_id = req.query?.restaurant_id;
-
-  // Get the data from the API, search for all the food that belongs to the restaurant with the restaurant_id
+  // clean restaurant_id of the quotes and spaces
+  restaurant_id = restaurant_id.replace(/['"]+/g, '').trim();
   try {
+    await databaseConnection();
     const restaurantInfo = await Restaurant.findOne({ _id: restaurant_id });
     res.status(200).json(restaurantInfo);
   } catch (error) {
