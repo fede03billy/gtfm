f712e695045683672667d377201f5da6ae698f30
Federico C <fede03billy@gmail.com>
2023-04-11 11:17:26 +0200

    🆙 Added spacial categories selectors

--- components/foodList.js
+++ components/foodList.js
@@ -14,16 +14,74 @@ export default function FoodList(props) {
     changeCategory(food[0].category);
   }, []);
 
+  function specialCategory(item, index) {
+    // // switch case for the special categories *** DIDN'T WORK ***
+    // switch (category.category) {
+    //   // find the various case in components/categories.js
+    //   case 'Senza lattosio':
+    //     if (item.lactose_free) {
+    //       return <FoodItem item={item} key={index} />;
+    //     }
+    //   case 'Senza glutine':
+    //     if (item.gluten_free) {
+    //       return <FoodItem item={item} key={index} />;
+    //     }
+    //   case 'Vegano':
+    //     if (item.vegan) {
+    //       return <FoodItem item={item} key={index} />;
+    //     }
+    //   default:
+    //     return null;
+    // }
+    if (category.category === 'Senza lattosio') {
+      if (item.lactose_free) {
+        return <FoodItem item={item} key={index} />;
+      }
+    } else if (category.category === 'Senza glutine') {
+      if (item.gluten_free) {
+        return <FoodItem item={item} key={index} />;
+      }
+    } else if (category.category === 'Vegano') {
+      if (item.vegan) {
+        return <FoodItem item={item} key={index} />;
+      }
+    } else {
+      return null;
+    }
+  }
+
   return (
     <div
       id="foodListContainer"
       className="relative foodListContainer overflow-y-auto overscroll-contain overflow-hidden scroll-smooth px-4"
     >
       {food.map((item, index) => {
-        if (item.category === category.category) {
+        // if the category is one of the special categories, call the specialCategory function
+        if (
+          category.category === 'Senza lattosio' ||
+          category.category === 'Senza glutine' ||
+          category.category === 'Vegano'
+        ) {
+          return specialCategory(item, index);
+        } else if (item.category === category.category) {
           return <FoodItem item={item} key={index} />;
         }
       })}
+      {food.filter((item) => {
+        if (
+          category.category === 'Senza lattosio' ||
+          category.category === 'Senza glutine' ||
+          category.category === 'Vegano'
+        ) {
+          return specialCategory(item);
+        } else {
+          return item.category === category.category;
+        }
+      }).length === 0 && (
+        <p className="text-md text-black opacity-30 mt-[40vh] w-full text-center font-thin">
+          Non c'è nulla in questa categoria.
+        </p>
+      )}
     </div>
   );
 }
