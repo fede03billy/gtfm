95a6b91deacd6d26bfcfbc87c5d67688eb84317b
Federico C <fede03billy@gmail.com>
2023-04-14 17:57:04 +0200

    🚧 Loading while serverside rendering

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
@@ -11,7 +13,36 @@ module.exports = {
     fontFamily: {
       helvetica: ['Helvetica', 'sans-serif'],
     },
-    extend: {},
+    extend: {
+      animation: {
+        fade: 'fade 1.4s infinite both',
+      },
+      keyframes: {
+        fade: {
+          '0%, 100%': {
+            opacity: '1',
+          },
+          '50%': {
+            opacity: ' 0.3',
+          },
+        },
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(({ matchUtilities, theme }) => {
+      matchUtilities(
+        {
+          'animation-delay': (value) => {
+            return {
+              'animation-delay': value,
+            };
+          },
+        },
+        {
+          values: theme('transitionDelay'),
+        }
+      );
+    }),
+  ],
 };
